8b9083c38795102922a26d14221683bbe53d3d51
Kai Skibbe <kai.skibbe@gmx.de>
2013-03-29 12:42:31 +0100

    Initial state

---
+++ src/HelloWorld.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : Kai
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	auto value = 5.0f;
+	cout << "Hello Kai, value is " << value << endl; // prints Hello Kai
+	return 0;
+}
